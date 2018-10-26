756c99384a547e9acf75c02d905e16388b62206c
AlvaroCabreraDam2 <43619912+AlvaroCabreraDam2@users.noreply.github.com>
2018-10-26 18:27:55 +0200

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -111,8 +111,9 @@ function deleteEnemmy(type) {
     choseObstacle();
 }
 
-// Detector de colisiones
+// Detector de colisiones (Devuelve true si colisionan)
 var overlaps = (function () {
+
     function getPositions( elem ) {
         var pos, width, height;
         pos = $( elem ).position();
@@ -122,17 +123,17 @@ var overlaps = (function () {
     }
 
     function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
+        var r1 = p1[0] < p2[0] ? p1 : p2;
+        var r2 = p1[0] < p2[0] ? p2 : p1;
         return r1[1] > r2[0] || r1[0] === r2[0];
     }
 
     return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
+        var pos1 = getPositions( a );
+        var pos2 = getPositions( b );
         return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
     };
+    
 })();
 
 // Christian Cosas que no pero si 
